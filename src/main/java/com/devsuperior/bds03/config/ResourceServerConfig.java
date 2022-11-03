34b84c08bcf1d16b5fc13ba671804e32c37bf42a
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2022-11-03 10:01:32 -0300

    Tests passing

--- src/main/java/com/devsuperior/bds03/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/bds03/config/ResourceServerConfig.java
@@ -23,8 +23,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	private JwtTokenStore tokenStore;
 
 	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
-	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
-	private static final String[] ADMIN = { "/users/**" };
+	private static final String[] OPERATOR_GET = { "/departments/**", "/employees/**" };
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -41,10 +40,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
-		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
-		.antMatchers(ADMIN).hasRole("ADMIN")
-		.anyRequest().authenticated();
+		.antMatchers(HttpMethod.GET, OPERATOR_GET).hasAnyRole("OPERATOR", "ADMIN")
+		.anyRequest().hasAnyRole("ADMIN");
 	}
 
 }
